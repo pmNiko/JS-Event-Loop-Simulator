6159f43124e21d20676ab28e9152bc66a8749947
pmNiko <pmNiko@users.noreply.github.com>
2025-10-19 21:57:23 +0000

    deploy: 9c793094b584cf374233e5ac4611cd00482b1d27

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'event-loop-sim-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/favicon.svg',
+  '/manifest.webmanifest',
+  // Vite genera los assets en /assets, cacheamos todo lo esencial
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
